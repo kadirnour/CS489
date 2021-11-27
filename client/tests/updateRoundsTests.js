0565a39d9517f6aae7c212bbccb0fbea2a440dc7
Ayleah Hill <ayleah.hill@outlook.com>
2021-11-27 15:49:31 -0800

    making tests

--- client/tests/updateRoundsTests.js
+++ client/tests/updateRoundsTests.js
@@ -1,1 +1,15 @@
 import { Selector } from 'testcafe';
+
+fixture `updateRoundsTests`
+    .page `http://localhost:3000/`;
+
+test('updateRoundsTests', async t => {
+    await t
+
+        .typeText(Selector('#email'), 'AKH@x.com')
+        .typeText(Selector('#password'), 'Password1')
+        .click("#loginBtn")
+        .expect(Selector('#feedModeTab').visible).eql(true)
+        .click("#profileBtn")
+  
+});
