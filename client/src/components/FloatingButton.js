38761bc474c6a641820f87d8a892de4a94bf726e
Ayleah Hill <ayleah.hill@outlook.com>
2021-12-04 16:16:06 -0800

    completed floating button from class to hook

--- client/src/components/FloatingButton.js
+++ client/src/components/FloatingButton.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class FloatingButton extends React.Component {
-    
-    render() {
+function FloatingButton(props) {
        return (
         <button id="roundsModeActionBtn"  type="button" 
-                className="float-btn" onClick={this.props.action}>
-          <FontAwesomeIcon icon={this.props.icon}/>
-            &nbsp;{this.props.label}
+                className="float-btn" onClick={props.action}>
+          <FontAwesomeIcon icon={props.icon}/>
+            &nbsp;{props.label}
         </button>
     ); 
-  }
 }
 
 export default FloatingButton;
