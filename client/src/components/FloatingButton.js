3a1d535776648db45bbb2a797e27c88bf51be6f6
Ayleah Hill <ayleah.hill@outlook.com>
2021-12-07 15:30:53 -0800

    comment block for FloatingButton()

--- client/src/components/FloatingButton.js
+++ client/src/components/FloatingButton.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+/**
+ * FloatingButton():
+ * Renders the floating button on the Round Page to log a new round.
+ * @param {props} the inherited properties of current state.
+ */
 function FloatingButton(props) {
        return (
         <button id="roundsModeActionBtn"  type="button" 
