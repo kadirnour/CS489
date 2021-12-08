c94817c6fcce669594f9f74b7918cc63df958b50
zpenn <zachpenn@live.com>
2021-12-07 19:05:35 -0800

    change buddiespage to a function

--- client/src/components/BuddiesPage.js
+++ client/src/components/BuddiesPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import logo from '../images/sslogo2.png'
 
-class BuddiesPage extends React.Component {
+function BuddiesPage() {
 
-    render() {
-        return ( 
+        return (
             <div id="buddiesModeTab" className="mode-page" role="tabpanel"
                  aria-label="Buddies Tab" tabIndex="0">
                 <h1 className="mode-page-header">Buddies</h1>
@@ -13,7 +12,6 @@ class BuddiesPage extends React.Component {
                      src={logo} alt="SpeedScore logo"></img>
             </div>
         );
-    }   
 }
 
 export default BuddiesPage;
